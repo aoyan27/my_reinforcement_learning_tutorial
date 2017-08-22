ff1232f76f31f958659ea75b8d851a37c6157c03
aoyan27 <ce62001@meiji.ac.jp>
2017-08-23 04:21:47 +0900

    added deep_q_learning code for pendulum. But it hasn't been going well.

--- scripts/deep_q_learning/agents/pendulum_agent.py
+++ scripts/deep_q_learning/agents/pendulum_agent.py
@@ -11,21 +11,154 @@
 
 import numpy as np
 
-class Agent:
-    ALPHA = 0.5
-    GAMMA = 0.9
-    
+import chainer
+from chainer import cuda, Function, gradient_check, Variable, optimizers, serializers, utils
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+import chainer.initializers as I
 
-    def __init__(self, num_state, num_action, ):
-        self.__epsilon = 0.1
+import copy
+
+class ActionValueNetwork(Chain):
+    def __init__(self, n_state, n_action):
+        super(ActionValueNetwork, self).__init__(
+            l1=L.Linear(n_state, 100),
+            l2=L.Linear(100, 100),
+            l3=L.Linear(100, 100),
+            l4=L.Linear(100, n_action, initialW=np.zeros((n_action, 100), dtype=np.float32)),
+        )
         
 
-if __name__=="__main__":
-    from math import cos, sin
-    theta = 1.0 #[rad]
-    thetadot = 0.5 #[rad/s]
-    num_state = len(np.array([cos(theta), sin(theta), thetadot]))
-    print np.array([cos(theta), sin(theta), thetadot])
-    num_action = len(np.arange(-1, 2, 2))
-    print np.arange(-1, 2, 2)
-    test_agent = Agent(num_state, num_action)
+    def __call__(self, state, gpu=-1):
+        if gpu >= 0:
+            state = cuda.to_gpu(state)
+        x = Variable(state)
+        h = F.relu(self.l1(x))
+        h = F.relu(self.l2(h))
+        h = F.relu(self.l3(h))
+        y = self.l4(h)
+        return y
+
+class Agent:
+    ALPHA = 1e-6
+    GAMMA = 0.9
+
+    def __init__(self, n_state, n_action, gpu):
+        self.n_state = n_state
+        self.n_action = n_action
+        self.action_list = np.arange(-1, 2, 1)
+
+        self.gpu = gpu
+
+        self.model = ActionValueNetwork(self.n_state, self.n_action)
+        if self.gpu >= 0:
+            self.model.to_gpu()
+
+        self.optimizer = optimizers.Adam()
+        self.optimizer.setup(self.model)
+
+        #  self.epsilon = 1.0
+        self.epsilon = 0.1
+        self.epsilon_decay = 0.00001
+        self.min_epsilon = 0.1
+
+    def forward(self, state, next_state, action, reward, episode_end):
+        Q = self.model(state, self.gpu)
+        #  print "Q : ", Q.data
+        tmp = self.model(next_state, self.gpu)
+        tmp = map(np.max, tmp.data)
+        #  print "tmp : ", tmp
+        max_next_Q = np.array(tmp, dtype=np.float32)
+        #  print "max_next_Q : ", max_next_Q
+        target = copy.deepcopy(Q.data)
+        #  print "target : ", target
+
+        if not episode_end:
+            tmp_ = reward + self.GAMMA*max_next_Q
+        else:
+            tmp_ = reward
+        #  print "tmp_ : ", tmp_
+        #  print "action : ", action
+        #  print "np.where(self.action_list == action) : ", np.where(self.action_list == action)
+        action_index = np.where(self.action_list == action)[0][0]
+        #  print "action_index : ", action_index
+        #  print "target : ", target[0][action_index]
+        target[0][action_index] = tmp_[0]
+        #  print "target : ", target
+
+        td = Variable(target) - Q
+        #  print "td : ", td.data
+
+        zero_val = np.zeros((1, self.n_action), dtype=np.float32)
+        if self.gpu >= 0:
+            zero_val = cuda.to_gpu(zero_val)
+        zero_val = Variable(zero_val)
+        #  print "zero_val : ", zero_val.data
+
+        loss = F.mean_squared_error(td, zero_val)
+        #  print "loss : ", loss.data
+
+        return loss, Q
+
+    def train(self, state, next_state, action, reward, episode_end):
+        self.model.cleargrads()
+        loss, _ = self.forward(state, next_state, action, reward, episode_end)
+        loss.backward()
+        self.optimizer.update()
+
+    def get_action(self, state, evaluation=False):
+        if not evaluation:
+            if np.random.rand() < self.epsilon:
+                #  print "Rondom action!!"
+                return self.action_list[np.random.randint(0, self.n_action)], 0
+            else:
+                Q = self.model(state, self.gpu)
+                #  print "Q(action) : ", Q.data
+                Q = Q.data[0]
+                #  print "Q(action) : ", Q
+                action = self.action_list[int(np.argmax(Q))]
+                #  print "action : ", action
+                return action, Q.max()
+        else:
+            Q = self.model(state, self.gpu)
+            #  print "Q(action) : ", Q.data
+            Q = Q.data[0]
+            #  print "Q(action) : ", Q
+            action = self.action_list[np.argmax(Q)]
+            #  print "action : ", action
+            return action, Q.max()
+    
+    def save_model(self, path):
+        if self.gpu >= 0:
+            self.model.to_cpu()
+        print "Save model to {}".format(path)
+        serializers.save_npz(path, self.model)
+
+
+if __name__ == "__main__":
+    import gym
+    env = gym.make('Pendulum-v0')
+    num_state = len(env.observation_space.high)
+    num_action = 3
+    gpu = -1
+
+    test_agent = Agent(num_state, num_action, gpu)
+    action_list = np.array([-1, 0, 1], dtype=np.int32)
+    np.random.shuffle(action_list)
+    action = np.array([action_list[0]])
+    print "action : ", action
+    state = env.reset()
+    print "state : ", state
+    next_state, reward, episode_end, _ = env.step(action)
+    print "next_state : ", next_state
+
+    state = np.array([state], dtype=np.float32)
+    next_state = np.array([next_state], dtype=np.float32)
+
+    test_agent.forward(state, next_state, action, reward, episode_end)
+
+    test_agent.train(state, next_state, action, reward, episode_end)
+
+    action, Q_max = test_agent.get_action(state)
+    print "action : ", action, " Q_max : ", Q_max
--- scripts/deep_q_learning/pendulum_main.py
+++ scripts/deep_q_learning/pendulum_main.py
@@ -9,40 +9,75 @@
         reward = -1.0 * (theta**2 + 0.1 * theta_dot**2 + 0.001*max_toruque**2)
 """
 
+import argparse
+
 import numpy as np
 import gym
+import sys
 from agents.pendulum_agent import Agent
 
-def main():
-    env = gym.make('Pendulum-v0')
-    #  print "env.action_space : ", env.action_space
-    #  print "env.action_space.high : ", env.action_space.high
-    #  print "env.action_space.low : ", env.action_space.low
-    #  print "env.observation_space : ", env.observation_space
-    #  print "env.observation_space.high : ", env.observation_space.high
-    #  print "env.observation_space.low : ", env.observation_space.low
-    num_state = len(env.observation_space.high)
-    num_action = 2
-    agent = Agent(num_state, num_action)
+import time
 
-    max_episode = 3000
+def main(max_episode, gpu, evaluation):
+    env_name = 'Pendulum-v0'
+    env = gym.make(env_name)
+
     max_step= 200
+    
+    success = 0
+    failure = 0
+
+    num_state = len(env.observation_space.high)
+    num_action = 3
+    
+    agent = Agent(num_state, num_action, gpu)
+
+    path = "/home/amsl/my_reinforcement_learning_tutorial/models/deep_q_learning/"
+
+    if not evaluation:
+        print "Train mode!!!"
+    else:
+        print "evaluation mode!!!"
+
 
     for i_episode in xrange(max_episode):
-        print "{} spisode".format(i_episode+1)
         state = env.reset()
         for t in xrange(max_step):
-            env.render()
-            action = env.action_space.sample()
-            next_state, reward, done, _ = env.step(action)
-            print "state : ", state, " action : ", action, " next_state : ", next_state, " reward : ", reward, " done : ", done
+            #  env.render()
+            #  action = env.action_space.sample()
+            action, Q_max = agent.get_action(np.array([state], dtype=np.float32))
+            next_state, reward, done, _ = env.step(np.array([action]))
+
+            if not evaluation:
+                agent.train(np.array([state], dtype=np.float32), np.array([next_state], dtype=np.float32), np.array([action], dtype=np.int32), np.array([reward], dtype=np.float32), done)
 
             if done:
-                print "Episode is ended. ({}time steps)".format(t+1)
+                if reward > 0:
+                    success += 1
+                else:
+                    failure += 1
+
+                sys.stdout.write("\repisode : {0:4d} success : {1:4d} failure : {2:4d} success rate : {3:0.3f}".format(i_episode, success, failure, float(success)/float(success+failure)))
+                sys.stdout.flush()
                 break
             else:
                 state = next_state
 
+    print "Success : ", success
+    print "Failure : ", failure
+    print "Success Rate : ", success / float(max_episode)
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='This script is ...')
+    parser.add_argument('-i', '--iteration', help='max iteration of episode', type=int, default=1000000)
+    parser.add_argument('--gpu', help='gpu mode(0) or cpu mode(-1)', type=int, default=-1)
+    parser.add_argument('-e', '--evaluation', help='evaluation mode', type=bool, default=False)
+
+    args = parser.parse_args()
+    if args.gpu >= 0:
+        print "GPU mode!!"
+    else:
+        print "CPU mode!!"
+
+    main(args.iteration, args.gpu, args.evaluation)
