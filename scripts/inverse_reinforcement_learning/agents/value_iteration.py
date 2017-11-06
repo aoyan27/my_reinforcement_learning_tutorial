cc7e0d425a8fccc3d374943a18972e4e34194120
aoyan <ce62001@meiji.ac.jp>
2017-11-07 03:33:28 +0900

    modified state2index function and index2state function

--- scripts/inverse_reinforcement_learning/agents/value_iteration.py
+++ scripts/inverse_reinforcement_learning/agents/value_iteration.py
@@ -16,16 +16,16 @@ class ValueIterationAgent:
 
         self.P_a = P_a
 
-        self.policy = []
+        self.policy = None
 
 
-    def train(self, reward_map, threshold=0.001):
+    def train(self, reward_map, threshold=0.01):
         R  = reward_map
-        print "R : "
-        print R
+        #  print "R : "
+        #  print R
         count = 0
         while 1:
-            print "count : ", count
+            #  print "count : ", count
             V_ = copy.deepcopy(self.V)
             delta = 0
             for s in xrange(self.env.n_state):
@@ -35,30 +35,60 @@ class ValueIterationAgent:
                 state = self.env.index2state(s)
                 #  print "state : ", state
                 #  print "R[state] : ", R[state[0], state[1]]
-                self.V[s] = R[state[0], state[1]] + \
-                        self.gamma*max([sum(self.P_a[s, s_dash, a]*self.V[s_dash] \
-                        for s_dash in xrange(self.env.n_state)) \
+                #  self.V[s] = R[state[0], state[1]] + \
+                        #  self.gamma*max([sum(self.P_a[s, s_dash, a]*self.V[s_dash] \
+                        #  for s_dash in xrange(self.env.n_state)) \
+                        #  for a in xrange(self.env.n_action)])
+                self.V[s] = max([sum([self.P_a[s, s_dash, a]*(R[s] + self.gamma*V_[s_dash]) \
+                        for s_dash in xrange(self.env.n_state)]) \
                         for a in xrange(self.env.n_action)])
 
+                #  print "sum[...] : "
+                #  print [sum([self.P_a[s, s_dash, a]*(R[s] + self.gamma*V_[s_dash]) for s_dash in xrange(self.env.n_state)]) for a in xrange(self.env.n_action)]
+                #  print "max : "
+                #  print max([sum([self.P_a[s, s_dash, a]*(R[s] + self.gamma*V_[s_dash]) for s_dash in xrange(self.env.n_state)]) for a in xrange(self.env.n_action)])
+                #  print "self.V : "
+                #  print self.V
             #  print "self.V : "
             #  print self.V.reshape([5,5])
             count += 1
             if max(abs(self.V - V_)) < threshold:
+                #  print "self.P_a[0] : "
+                #  print self.P_a[0]
+                #  print "R : "
+                #  print R
+                #  print "self.V : "
+                #  print self.V
                 break
 
-    def get_policy(self, reward_map):
+    def get_policy(self, reward_map, deterministic=True):
         R = reward_map
-        for s in xrange(self.env.n_state):
-            state = self.env.index2state(s)
-            opt_action = np.argmax([sum(self.P_a[s, s_dash, a] *\
-                    (R[state[0], state[1]]+self.gamma*self.V[s_dash]) \
-                    for s_dash in xrange(self.env.n_state)) \
-                    for a in xrange(self.env.n_action)])
-            self.policy.append(opt_action)
+        if deterministic:
+            self.policy = np.zeros([self.env.n_state])
+            for s in xrange(self.env.n_state):
+                state = self.env.index2state(s)
+                self.policy[s] = np.argmax([sum(self.P_a[s, s_dash, a] *\
+                        (R[s]+self.gamma*self.V[s_dash]) \
+                        for s_dash in xrange(self.env.n_state)) \
+                        for a in xrange(self.env.n_action)])
+            #  print "self.policy : "
+            #  print self.policy
+        else:
+            state_value = []
+            self.policy = np.zeros([self.env.n_state, self.env.n_action])
+            for s in xrange(self.env.n_state):
+                state = self.env.index2state(s)
+                state_value = [sum(self.P_a[s, s_dash, a] *\
+                        (R[s]+self.gamma*self.V[s_dash]) \
+                        for s_dash in xrange(self.env.n_state)) \
+                        for a in xrange(self.env.n_action)]
+                #  print "state_value : "
+                #  print state_value
+                self.policy[s, :] = state_value/np.sum(state_value)
+                #  print "policy : "
+                #  print policy[s, :]
+                
 
-        self.policy = np.asarray(self.policy)
-        #  print "self.policy : "
-        #  print self.policy
 
 
 if __name__=="__main__":
@@ -81,6 +111,7 @@ if __name__=="__main__":
 
     reward_map = np.zeros([rows, cols])
     reward_map[rows-1, cols-1] = R_max
+    reward_map = np.reshape(reward_map, rows*cols)
 
     agent = ValueIterationAgent(env, P_a, gamma)
     print "agent.n_state : ", agent.env.n_state
@@ -91,11 +122,13 @@ if __name__=="__main__":
     print "agent.V : "
     print agent.V.reshape([rows, cols])
 
-    agent.get_policy(reward_map)
+    agent.get_policy(reward_map, deterministic=False)
+    #  agent.get_policy(reward_map, deterministic=True)
     print "agent.policy : "
-    print agent.policy.reshape([rows, cols])
+    print agent.policy
 
-    env.show_policy(agent.policy.reshape(-1))
+    env.show_policy(agent.policy, deterministic=False)
+    #  env.show_policy(agent.policy, deterministic=True)
 
 
     
