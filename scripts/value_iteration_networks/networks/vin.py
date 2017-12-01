d00bb11062af6447874cbc439354ead7bebaa4e6
aoyan <ce62001@meiji.ac.jp>
2017-12-02 04:52:30 +0900

    added VIN train script

--- scripts/value_iteration_networks/networks/vin.py
+++ scripts/value_iteration_networks/networks/vin.py
@@ -9,7 +9,7 @@ from chainer import Chain
 import chainer.functions as F
 import chainer.links as L
 
-class ValueIterationNetwork(chain):
+class ValueIterationNetwork(Chain):
     def __init__(self, n_in=2, l_h=150, l_q=8, n_out=8, k=10):
         super(ValueIterationNetwork, self).__init__(
             conv1 = L.Convolution2D(n_in, l_h, 3, stride=1, pad=1), 
@@ -18,11 +18,76 @@ class ValueIterationNetwork(chain):
             conv3a = L.Convolution2D(1, l_q, 3, stride=1, pad=1, nobias=True),
             conv3b = L.Convolution2D(1, l_q, 3, stride=1, pad=1, nobias=True),
 
-            l4 = L.Linear(l_q, l_a, nobias=True),
+            l4 = L.Linear(l_q, n_out, nobias=True),
         )
 
         self.k = k
+    
+    def attention(self, q, state_list):
+        #  print q.data.shape
+        w = np.zeros(q.data.shape)
+        #  print "w : ", w.shape
+        for i in xrange(len(state_list)):
+            #  print "state_list : ", state_list[i]
+            w[i, :, int(state_list[i][0]), int(state_list[i][1])] = 1.0
+            #  print "w : "
+            #  print w[i]
 
-    def __call__(self, x, state_x, state_y):
-        pass
+        if isinstance(q.data, cuda.ndarray):
+            w = cuda.to_gpu(w)
+
+        #  print q.data.shape
+        #  print w.shape
+        
+        w = Variable(w.astype(np.float32))
+        #  print "state_list : "
+        #  print state_list[0]
+        a = q * w
+        #  print "a : "
+        #  print a.shape
+        a = F.reshape(a, (a.data.shape[0], a.data.shape[1], -1))
+        #  print "a() : "
+        #  print a.shape
+
+        q_out = F.sum(a, axis=2)
+        #  print "q_out : "
+        #  print q_out.shape
+        return q_out
+
+
+    def __call__(self, input_data, state_list, action_list):
+        input_data = Variable(input_data.astype(np.float32))
+
+        h = F.relu(self.conv1(input_data))
+        self.r = self.conv2(h)
+        #  print "self.r : ", self.r.data.shape
+
+        q = self.conv3a(self.r)
+        #  print "q : ", q.data.shape
+        
+        self.v = F.max(q, axis=1, keepdims=True)
+        #  print "self.v : ", self.v.shape
+
+        for i in xrange(self.k):
+            q = self.conv3a(self.r) + self.conv3b(self.v)
+            self.v = F.max(q, axis=1, keepdims=True)
+
+        #  print "q(after k) : ", q.shape
+        #  print "self.v : ", self.v.shape
+        
+        q = self.conv3a(self.r) + self.conv3b(self.v)
+        q_out = self.attention(q, state_list)
+
+        y = self.l4(q_out)
+
+        return y
+
+    def forward(self, input_data, state_list, action_list):
+        y = self.__call__(input_data, state_list, action_list)
+        
+        t = Variable(action_list.astype(np.int32))
+
+        return F.softmax_cross_entropy(y, t), F.accuracy(y, t)
+
+
 
