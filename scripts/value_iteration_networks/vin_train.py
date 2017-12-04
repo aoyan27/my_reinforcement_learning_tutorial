94f441a01aa9291ab8267e54c1df8c6005c4fdb8
aoyan <ce62001@meiji.ac.jp>
2017-12-04 22:28:57 +0900

    modified network architecture

--- scripts/value_iteration_networks/vin_train.py
+++ scripts/value_iteration_networks/vin_train.py
@@ -189,8 +189,8 @@ def main(dataset, n_epoch, batchsize, gpu, model_path):
     
 
     #  model = ValueIterationNetwork(l_q=5, n_out=5, k=20)
-    #  model = ValueIterationNetwork(l_q=9, n_out=9, k=20)
-    model = ValueIterationNetwork(l_h=200, l_q=9, n_out=9, k=20)
+    model = ValueIterationNetwork(l_q=9, n_out=9, k=20)
+    #  model = ValueIterationNetwork(l_h=200, l_q=9, n_out=9, k=20)
     if gpu >= 0:
         cuda.get_device(gpu).use()
         model.to_gpu()
