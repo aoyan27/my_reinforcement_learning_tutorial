3e4d5bfeb0cec28fafa24da061c718c03a42edb0
aoyan27 <ce62001@meiji.ac.jp>
2017-12-07 18:20:26 +0900

    modified attention_module

--- scripts/value_iteration_networks/networks/vin2.py
+++ scripts/value_iteration_networks/networks/vin2.py
@@ -31,7 +31,8 @@ class ValueIterationNetwork(Chain):
         #  print "v[0][0] : "
         #  print v.data[0][0]
         extract_size = [5, 5]
-        attention_grid = np.zeros((v.shape[0], v.shape[1], extract_size[0], extract_size[1]), dtype=np.float32)
+        attention_grid = \
+            np.zeros((v.shape[0], v.shape[1], extract_size[0], extract_size[1]), dtype=np.float32)
         #  print "attention_grid : "
         #  print attention_grid.shape
         #  print attention_grid
@@ -47,16 +48,39 @@ class ValueIterationNetwork(Chain):
             #  print "i : ", i
             y, x = state_list[i]
             #  print "y, x : ", y, x
-            for l_y in xrange(extract_size[0]):
-                for l_x in xrange(extract_size[1]):
-                    g_y = int(y + (l_y - center_y))
-                    g_x = int(x + (l_x - center_x))
-                    #  print "g_y : ", g_y
-                    #  print "g_x : ", g_x 
-                    if (g_x < 0 or v.shape[2]-1 < g_x) or (g_y < 0 or v.shape[3]-1 < g_y):
-                        attention_grid[i][0][l_y, l_x] = 0
-                    else:
-                        attention_grid[i][0][l_y, l_x] = v.data[i][0][g_y, g_x]
+            min_y = int(y - center_y)
+            max_y = int(y + center_y) + 1
+            min_x = int(x - center_x)
+            max_x = int(x + center_x) + 1
+            if min_y < 0:
+                min_y = 0
+            if v.shape[2] < max_y:
+                max_y =  v.shape[2]
+            if min_x < 0:
+                min_x = 0
+            if v.shape[3] < max_x:
+                max_x = v.shape[3]
+            #  print "min_y, max_y : ", min_y, max_y
+            #  print "min_x, max_x : ", min_x, max_x
+            #  v.data[i][0][min_y:max_y, min_x:max_x]
+            #  print "v.data[i][0][min_y:max_y, min_x:max_x] : "
+            #  print v.data[i][0][min_y:max_y, min_x:max_x]
+            
+            diff_min_y = min_y - y
+            diff_max_y = max_y - y
+            diff_min_x = min_x - x
+            diff_max_x = max_x - x
+
+            attention_min_y = int(center_y + diff_min_y)
+            attention_max_y = int(center_y + diff_max_y)
+            attention_min_x = int(center_x + diff_min_x)
+            attention_max_x = int(center_x + diff_max_x)
+
+            attention_grid[i][0][attention_min_y:attention_max_y, attention_min_x:attention_max_x]\
+                = v.data[i][0][min_y:max_y, min_x:max_x]
+            #  print "attention_grid[i][0] : "
+            #  print attention_grid[i][0]
+
         #  print "state_list[0] : ", state_list[0]
         #  print "attention_grid() : "
         #  print attention_grid.shape
