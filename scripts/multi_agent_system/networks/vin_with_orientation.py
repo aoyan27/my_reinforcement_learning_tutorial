e55b6c31169e8259067be58b50a144980beb2726
aoyan <ce62001@meiji.ac.jp>
2017-12-26 08:35:53 +0900

    added multi_agent_vin_train_with_orientation.py and vin network archtecture

---
+++ scripts/multi_agent_system/networks/vin_with_orientation.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+#coding : utf-8
+
+import numpy as np
+np.set_printoptions(precision=1, suppress=True, threshold=np.inf)
+
+import chainer 
+from chainer import cuda, Variable, optimizers, serializers
+from chainer import Chain
+import chainer.functions as F
+import chainer.links as L
+
+class ValueIterationNetwork(Chain):
+    def __init__(self, n_in=2, l_h=150, l_q=5, n_out=5, k=10):
+        super(ValueIterationNetwork, self).__init__(
+            conv1 = L.Convolution2D(n_in, l_h, 3, stride=1, pad=1), 
+            conv2 = L.Convolution2D(l_h, 1, 1, stride=1, pad=0, nobias=True),
+
+            conv3a = L.Convolution2D(1, l_q, 3, stride=1, pad=1, nobias=True),
+            conv3b = L.Convolution2D(1, l_q, 3, stride=1, pad=1, nobias=True),
+
+            l4 = L.Linear(None, 512, nobias=True),
+            l5 = L.Linear(512, 258, nobias=True),
+            l6 = L.Linear(258, n_out, nobias=True),
+        )
+
+        self.k = k
+    
+    def attention(self, q, state_list):
+        #  print "q.data : ",
+        #  print q.data[0]
+        w = np.zeros(q.data.shape)
+        #  print "w : ", w.shape
+        for i in xrange(len(state_list)):
+            #  print "state_list : ", state_list[i]
+            w[i, :, int(state_list[i][0]), int(state_list[i][1])] = 1.0
+            #  print "w : "
+            #  print w[i]
+
+        if isinstance(q.data, cuda.ndarray):
+            w = cuda.to_gpu(w)
+
+        #  print q.data.shape
+        #  print w.shape
+        
+        w = Variable(w.astype(np.float32))
+        #  print "state_list : "
+        #  print state_list[0]
+        a = q * w
+        #  print "a : "
+        #  print a.shape
+        a = F.reshape(a, (a.data.shape[0], a.data.shape[1], -1))
+        #  print "a() : "
+        #  print a.shape
+
+        q_out = F.sum(a, axis=2)
+        #  print "q_out : "
+        #  print q_out
+        #  print q_out.shape
+        return q_out
+
+
+    def __call__(self, input_data, state_list, orientation_list):
+        input_data = Variable(input_data.astype(np.float32))
+
+        h = F.relu(self.conv1(input_data))
+        #  print "h : ", h
+        self.r = self.conv2(h)
+        #  print "self.r : ", self.r
+        #  print "self.r : ", self.r.data.shape
+
+        q = self.conv3a(self.r)
+        #  print "q : ", q.data.shape
+        
+        self.v = F.max(q, axis=1, keepdims=True)
+        #  print "self.v : ", self.v.shape
+
+        for i in xrange(self.k):
+            q = self.conv3a(self.r) + self.conv3b(self.v)
+            self.v = F.max(q, axis=1, keepdims=True)
+
+        #  print "q(after k) : ", q.shape
+        #  print "q(after k) : ", q
+        #  print "self.v : ", self.v
+        
+        q = self.conv3a(self.r) + self.conv3b(self.v)
+        q_out = self.attention(q, state_list)
+
+        #  print "q_out : ", q_out
+        #  print "orientation_list : ", orientation_list
+        #  orientation_ = F.expand_dims(orientation_list.astype(np.float32), axis=1)
+        orientation_ = orientation_list.astype(np.float32)
+        #  print "orientation_ : ", orientation_
+
+        h_in = F.concat((q_out, orientation_), axis=1)
+        #  print "h_in : ", h_in
+
+
+        h1 = self.l4(h_in)
+        h2 = self.l5(h1)
+        y = self.l6(h2)
+
+        return y
+
+    def forward(self, input_data, state_list, orientation_list, action_list):
+        y = self.__call__(input_data, state_list, orientation_list)
+        #  print "y : ", y
+        
+        t = Variable(action_list.astype(np.int32))
+
+        return F.softmax_cross_entropy(y, t), F.accuracy(y, t)
+
+
+
