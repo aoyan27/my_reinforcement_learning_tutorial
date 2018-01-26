e4018d1f10acaf4c3024acc5bad795e1c9da1f7e
aoyan <ce62001@meiji.ac.jp>
2018-01-26 23:29:09 +0900

    added make_dataset_multi_agent_object_world_with_velocity_and_orientation.py

---
+++ scripts/multi_agent_system/make_dataset_multi_agent_object_world_with_velocity_and_orientation.py
@@ -0,0 +1,442 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import argparse
+
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+from progressbar import ProgressBar
+
+import copy
+import pickle
+import time
+import math
+
+#  from envs.multi_agent_grid_world import Gridworld
+from envs.multi_agent_object_world import Objectworld
+from agents.a_star_agent import AstarAgent
+
+
+def grid2image(array):
+    image = copy.deepcopy(array)
+
+    index = np.where(image == 1)
+    for i in xrange(len(index[0])):
+        image[index[0][i], index[1][i]] = 0
+
+    index = np.where(image == -1)
+    for i in xrange(len(index[0])):
+        image[index[0][i], index[1][i]] = 1
+
+    return image
+
+def view_image(array, title):
+    image = cv.cvtColor(array.astype(np.uint8), cv.COLOR_GRAY2RGB)
+    #  print image
+    plt.imshow(255 - 255*image, interpolation="nearest")
+    plt.title(title)
+    plt.show()
+
+def get_input_image(env, state_list, action_list, n_agents, n_trajs):
+    domain_grid_list = {}
+    domain_agent_grid_list = {}
+    domain_another_agent_position_with_grid_list = {}
+    for agent_id in xrange(n_agents):
+        domain_grid_list[agent_id] = []
+        domain_agent_grid_list[agent_id] = []
+        domain_another_agent_position_with_grid_list[agent_id] = []
+
+    start_position = {}
+    goal_position = {}
+    action = {}
+    for i_traj in xrange(n_trajs):
+        #  print "++++++++++++++++++++++++++++++++++++++++++++++++++"
+        #  print "i_traj : ", i_traj
+        traj_grid_list = {}
+        traj_agent_grid_list = {}
+        traj_another_agent_position_with_grid_list = {}
+        for agent_id in xrange(n_agents):
+            traj_grid_list[agent_id] = []
+            traj_agent_grid_list[agent_id] = []
+            traj_another_agent_position_with_grid_list[agent_id] = []
+
+        max_step = len(state_list[0][i_traj])
+        #  print "max_step : ", max_step
+        for agent_id in xrange(n_agents):
+            start_position[agent_id] = state_list[agent_id][i_traj][0]
+            goal_position[agent_id] = state_list[agent_id][i_traj][max_step-1]
+        #  print "start_position : ", start_position
+        #  print "goal_position : ", goal_position
+        observation = env.reset(start_position=start_position, goal_position=goal_position)
+
+        for i_step in xrange(max_step):
+            for agent_id in xrange(n_agents):
+                traj_grid_list[agent_id].append(grid2image(env.grid))
+                traj_agent_grid_list[agent_id].append(grid2image(env.agent_grid[agent_id]))
+                another_agent_position_with_grid = np.zeros((env.rows, env.cols))
+                for another_agent_id in xrange(n_agents):
+                    #  print "agent_id : ", agent_id
+                    if another_agent_id != agent_id:
+                        #  print "another_agent_id : ", another_agent_id
+                        another_agent_position = state_list[another_agent_id][i_traj][i_step]
+                        #  print "another_agent_position : ", another_agent_position
+                        another_agent_position_with_grid[tuple(another_agent_position)] = -1
+                traj_another_agent_position_with_grid_list[agent_id].append(\
+                        grid2image(another_agent_position_with_grid))
+                #  print "traj_another_agent_position_with_grid_list : "
+                #  print traj_another_agent_position_with_grid_list
+
+                action[agent_id] = int(action_list[agent_id][i_traj][i_step])
+            #  print "action : ", action
+            observation, reward, episode_end, info = env.step(action)
+        #  print "traj_grid_list[0] : "
+        #  print traj_grid_list[0]
+
+        for agent_id in xrange(n_agents):
+            domain_grid_list[agent_id].append(traj_grid_list[agent_id])
+            domain_agent_grid_list[agent_id].append(traj_agent_grid_list[agent_id])
+            domain_another_agent_position_with_grid_list[agent_id].append(\
+                    traj_another_agent_position_with_grid_list[agent_id])
+    #  print "domain_grid_list[0] : "
+    #  print domain_grid_list[0]
+    #  print len(domain_grid_list[0])
+
+    grid_list = []
+    agent_grid_list = []
+    another_agent_position_with_grid_list = []
+
+    for agent_id in xrange(n_agents):
+        grid_list.append(domain_grid_list[agent_id])
+        agent_grid_list.append(domain_agent_grid_list[agent_id])
+        another_agent_position_with_grid_list.append(\
+                domain_another_agent_position_with_grid_list[agent_id])
+
+    return grid_list, agent_grid_list, another_agent_position_with_grid_list
+
+
+def get_reward_map(env, n_agents):
+    reward_map = np.zeros((n_agents, env.rows, env.cols))
+    for i in xrange(n_agents):
+        reward_map[i, env.goal[i][0], env.goal[i][1]] = 1.0
+    #  print "reward_map : "
+    #  print reward_map
+    return reward_map
+
+def get_agent_state_and_action(env, agent_id):
+    a_agent = AstarAgent(env, agent_id)
+    #  print "env.agent_grid[agent_id] : "
+    #  print env.agent_grid[agent_id]
+    #  print "env._state[agent_id] : ", env._state[agent_id]
+    #  a_agent.get_shortest_path(env._state[agent_id], env.grid)
+    a_agent.get_shortest_path(env._state[agent_id], env.agent_grid[agent_id])
+    #  print "a_agent.found : ", a_agent.found
+    if a_agent.found:
+        #  pass
+        #  print "a_agent.state_list : "
+        #  print a_agent.state_list
+        #  print "a_agent.shrotest_action_list : "
+        #  print a_agent.shortest_action_list
+        #  env.show_policy(a_agent.policy.transpose().reshape(-1))
+        path_data = a_agent.show_path()
+        #  print "agent_id : ", agent_id
+        #  print "view_path_my : "
+        #  a_agent.view_path(path_data['vis_path'])
+    #  print "a_agent.shortest_action_list[0] : "
+    #  print a_agent.shortest_action_list[0]
+    state_list = a_agent.state_list
+    action_list = a_agent.shortest_action_list
+
+    return state_list, action_list, a_agent.found
+
+def check_agent_collision(n_agents, state_list):
+    tmp_flag = None
+    for agent_id in xrange(n_agents):
+        agent_state_list = np.asarray(state_list[agent_id])
+        for another_agent_id in xrange(agent_id+1, n_agents):
+            another_agent_state_list = np.asarray(state_list[another_agent_id])
+            #  print "agent_state_list : "
+            #  print agent_state_list
+            #  print "another_agent_state_list : "
+            #  print another_agent_state_list
+
+            tmp_flag = agent_state_list != another_agent_state_list
+            #  print "tmp_flag : ", tmp_flag 
+
+    agent_collision_flag = []
+    ok_flag = [False, False]
+    for flag in tmp_flag:
+        agent_collision_flag.append(np.allclose(flag, ok_flag))
+    #  print "agent_collision_flag : ", agent_collision_flag
+
+    check_flag = None
+    if np.asarray(agent_collision_flag).any():
+        #  print "agent collision!!!!"
+        check_flag = False
+    else:
+        #  print "agent not collision!!!!"
+        check_flag =True
+
+    return check_flag
+
+
+def get_trajs(env, n_agents, n_trajs):
+    traj_state_list = {}
+    traj_action_list = {}
+    for agent_id in xrange(n_agents):
+        traj_state_list[agent_id] = []
+        traj_action_list[agent_id] = []
+
+    failed = False
+    j = 0
+    challenge_times = 0
+    found = [False for i in xrange(n_agents)]
+    for agent_id in xrange(n_agents):
+        found[agent_id] = False
+    while j < n_trajs:
+        #  print "-----------------------------------------------"
+        #  print "j : ", j
+        #  print "challenge_times : ", challenge_times
+        challenge_times += 1
+        if challenge_times > 50:
+            failed = True
+            break
+
+        env.set_start_random(check_goal=True)
+
+        domain_state_list = {}
+        domain_action_list = {}
+        step_count_list = []
+        for agent_id in xrange(n_agents):
+            #  print "agent_id : ", agent_id
+            #  env.show_objectworld_with_state()
+            domain_state_list[agent_id], domain_action_list[agent_id], found[agent_id] \
+                    = get_agent_state_and_action(env, agent_id)
+            #  print "found : ", found
+            step_count_list.append(len(domain_state_list[agent_id]))
+
+
+        #  print "all(found) : ", all(found)
+        if all(found):
+            #  print "step_count_list : ", step_count_list 
+            max_step_count = max(step_count_list)
+            #  print "max_step_count : ", max_step_count
+            for agent_id in xrange(n_agents):
+                diff_step = max_step_count - step_count_list[agent_id]
+                for i_step in xrange(diff_step):
+                    domain_state_list[agent_id].append(\
+                            domain_state_list[agent_id][step_count_list[agent_id]-1])
+                    domain_action_list[agent_id].append(\
+                            domain_action_list[agent_id][step_count_list[agent_id]-1])
+
+            if check_agent_collision(n_agents, domain_state_list):
+                for agent_id in xrange(n_agents):
+                    traj_state_list[agent_id].append(domain_state_list[agent_id])
+                    traj_action_list[agent_id].append(domain_action_list[agent_id])
+                #  print "traj_state_list : "
+                #  print traj_state_list
+                #  print "traj_action_list : "
+                #  print traj_action_list
+                j += 1
+                challenge_times = 0
+
+    state_list = []
+    action_list = []
+
+    for agent_id in xrange(n_agents):
+        state_list.append(traj_state_list[agent_id])
+        action_list.append(traj_action_list[agent_id])
+        
+    #  print "state_list : "
+    #  print state_list
+    #  print "action_list : "
+    #  print action_list
+
+    if failed:
+        del state_list[:]
+        del action_list[:]
+
+    return state_list, action_list
+
+def get_velocity_and_orientation(env, action_list):
+    #  print "action_list : "
+    #  print action_list
+
+    n_agent = len(action_list)
+    n_traj = len(action_list[0])
+    velocity = [[] for i in xrange(n_agent)]
+    orientation = [[] for i in xrange(n_agent)]
+    for i in xrange(n_agent):
+        traj_velocity = []
+        for j in xrange(n_traj):
+            velocity_ = [env.movement[a] for a in action_list[i][j]]
+            velocity[i].append(velocity_)
+            orientation_ \
+                = [math.atan2(env.movement[a][0], env.movement[a][1]) for a in action_list[i][j]]
+            orientation_size = len(orientation_)
+            no_stay_index = np.where(np.asarray(action_list[i][j]) != 8)
+            #  print "no_stay_index : ", no_stay_index[0][-1]
+            #  print orientation_[no_stay_index[0][-1]]
+            last_orientation = orientation_[no_stay_index[0][-1]]
+            for k in xrange(no_stay_index[0][-1]+1, orientation_size):
+                orientation_[k] = last_orientation
+            orientation[i].append(orientation_)
+            #  print "orientation_ : ", orientation_
+    #  print "velocity : ", velocity
+    #  print "orientation : ", orientation
+
+    return velocity, orientation
+
+
+def save_dataset(data, filename):
+    print "Save %d-%d multi_agent_map_dataset.pkl!!!!!" \
+            % (len(data['grid_image'][0]), len(data['grid_image'][1]))
+    with open(filename, mode='wb') as f:
+        pickle.dump(data, f)
+
+
+def main(rows, cols, n_objects, n_agents, n_domains, n_trajs, seed, save_dirs):
+    n_state = rows * cols
+    
+    goal = [rows-1, cols-1]
+    R_max = 1.0
+    noise = 0.0
+    mode = 1
+
+    env = Objectworld(rows, cols, n_objects, n_agents, noise, seed=seed, mode=mode)
+    #  print env.grid
+
+    #  print "env.n_state : ", env.n_state
+    #  print "env.n_action : ", env.n_action
+
+    #  print "env._state : ", env._state
+    #  print "env.goal : ", env.goal
+    
+    #  start = {0: [0, 0], 1: [rows-1, 0]}
+    #  goal = {0: [rows-1, cols-1], 1:[0, cols-1]}
+    #  env.set_start(start)
+    #  env.set_goal(goal)
+    
+    max_samples = (rows + cols) * n_domains * n_trajs
+    print "max_samples : ", max_samples
+
+    grid_image_data = np.zeros((n_agents, max_samples, rows, cols))
+    agent_grid_image_data = np.zeros((n_agents, max_samples, rows, cols))
+    another_agent_position_with_grid_image_data = np.zeros((n_agents, max_samples, rows, cols))
+
+    reward_map_data = np.zeros((n_agents, max_samples, rows, cols))
+
+    state_list_data = np.zeros((n_agents, max_samples, 2))
+    action_list_data = np.zeros((n_agents, max_samples))
+    velocity_list_data = np.zeros((n_agents, max_samples, 2))
+    orientation_list_data = np.zeros((n_agents, max_samples))
+    
+    #  print "image_data : ", image_data.shape
+    #  print "reward_map_data : ", reward_map_data.shape
+    #  print "state_list_data : ", state_list_data.shape
+    #  print "action_list_data : ", action_list_data.shape
+
+    prog = ProgressBar(0, n_domains)
+
+    dom = 0
+
+    num_sample = [0 for i in xrange(n_agents)]
+    while dom < n_domains:
+        #  print "===================================================="
+        #  print "dom : ", dom
+        env.set_goal_random(check_start=False)
+        env.set_objects()
+        #  print "env._state : ", env._state
+        #  print "env.goal : ", env.goal
+        #  print "env.grid : "
+        #  print env.grid
+
+        state_list, action_list = get_trajs(env, n_agents, n_trajs)
+
+        if len(state_list) == 0:
+            continue
+        velocity_list, orientation_list = get_velocity_and_orientation(env, action_list)
+
+        reward_map_list = get_reward_map(env, n_agents)
+        #  print "reward_map_list : "
+        #  print reward_map_list
+
+        grid_image_list, agent_grid_image_list, another_agent_position_with_grid_image_list\
+                = get_input_image(env, state_list, action_list, n_agents, n_trajs)
+        #  print "gri_image_list : "
+        #  print grid_image_list
+        #  view_image(image, 'Gridworld')
+
+
+        ns = 0
+        for j in xrange(n_agents):
+            #  print "num_sample[j] : ", num_sample[j]
+            #  print "j : ", j
+            #  print "len(state_list) : ", len(state_list)
+            
+            for i in xrange(n_trajs):
+                #  print "i : ", i
+                ns = len(state_list[j][i])
+                #  print "ns : ", ns
+                grid_image_data[j][num_sample[j]:num_sample[j]+ns] = grid_image_list[j][i][:]
+                agent_grid_image_data[j][num_sample[j]:num_sample[j]+ns] \
+                        = agent_grid_image_list[j][i][:]
+                another_agent_position_with_grid_image_data[j][num_sample[j]:num_sample[j]+ns] \
+                        = another_agent_position_with_grid_image_list[j][i][:]
+
+                reward_map_data[j][num_sample[j]:num_sample[j]+ns] = reward_map_list[j]
+                #  print "state_list : "
+                #  print state_list[j][i][:]
+                state_list_data[j][num_sample[j]:num_sample[j]+ns] = state_list[j][i][:]
+                action_list_data[j][num_sample[j]:num_sample[j]+ns] = action_list[j][i][:]
+                velocity_list_data[j][num_sample[j]:num_sample[j]+ns] = velocity_list[j][i][:]
+                orientation_list_data[j][num_sample[j]:num_sample[j]+ns] \
+                        = orientation_list[j][i][:]
+
+                num_sample[j] += ns
+
+        prog.update(dom)
+        dom += 1
+
+    
+    data = {'grid_image': [], 'agent_grid_image': [], 'another_agent_position': [], \
+            'reward': [], 'state': [], 'action': [], 'velocity': [], 'orientation': []}
+    for i in xrange(n_agents):
+        data['grid_image'].append(grid_image_data[i][0:num_sample[i]])
+        data['agent_grid_image'].append(agent_grid_image_data[i][0:num_sample[i]])
+        data['another_agent_position'].append(\
+                another_agent_position_with_grid_image_data[i][0:num_sample[i]])
+        data['reward'].append(reward_map_data[i][0:num_sample[i]])
+        data['state'].append(state_list_data[i][0:num_sample[i]])
+        data['action'].append(action_list_data[i][0:num_sample[i]])
+        data['velocity'].append(velocity_list_data[i][0:num_sample[i]])
+        data['orientation'].append(orientation_list_data[i][0:num_sample[i]])
+    
+    dataset_name ='multi_agent_object_world_velocity_orientation_map_dataset.pkl'
+    save_dataset(data, save_dirs+dataset_name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='This script is make_dataset_multi_agent ...')
+    
+    parser.add_argument('-r', '--rows', default=16, type=int, help='row of global gridworld')
+    parser.add_argument('-c', '--cols', default=16, type=int, help='column of global gridworld')
+
+    parser.add_argument('-o', '--n_objects', default=40, type=int, help='number of objects')
+    parser.add_argument('-a', '--n_agents', default=2, type=int, help='number of agents')
+    
+    parser.add_argument('-d', '--n_domains', default=5000, type=int, help='number of domains')
+    parser.add_argument('-t', '--n_trajs', default=10, type=int, help='number of trajs')
+    
+
+    parser.add_argument('-s', '--seed', default=0, type=int, help='number of seed')
+
+    parser.add_argument('-m', '--dataset_dirs', default='datasets/', \
+            type=str, help="save dataset directory")
+
+    args = parser.parse_args()
+    print args
+
+    main(args.rows, args.cols, args.n_objects, args.n_agents, args.n_domains, \
+            args.n_trajs, args.seed, args.dataset_dirs)
+    
