6e351c0ce8477d19adb04cf9395dae347111c9bd
aoyan27 <ce62001@meiji.ac.jp>
2018-01-29 12:43:56 +0900

    changed iteration num k=26(for 20x20 image)

--- scripts/value_iteration_networks/vin_train.py
+++ scripts/value_iteration_networks/vin_train.py
@@ -202,6 +202,7 @@ def main(dataset, n_epoch, batchsize, gpu, model_path):
     #  model = ValueIterationNetwork(l_q=9, n_out=9, k=20)
     model = ValueIterationNetwork(l_q=9, n_out=9, k=26)
     #  model = ValueIterationNetwork(l_q=9, n_out=9, k=36)
+    #  model = ValueIterationNetwork(l_q=9, n_out=9, k=50)
     #  model = ValueIterationNetwork(l_h=200, l_q=9, n_out=9, k=20)
     if gpu >= 0:
         cuda.get_device(gpu).use()
