b2d1e3f3761e8e643077b1aad8f6b7392c5910ca
aoyan27 <ce62001@meiji.ac.jp>
2018-01-31 20:07:43 +0900

    modified weight initializer

--- scripts/multi_agent_system/networks/multi_agent_vin_with_velocity_and_orientation_fine_tuning.py
+++ scripts/multi_agent_system/networks/multi_agent_vin_with_velocity_and_orientation_fine_tuning.py
@@ -21,9 +21,13 @@ class ValueIterationNetworkFineTuning(Chain):
             conv3b = L.Convolution2D(1, l_q, 3, stride=1, pad=1, \
                             initialW=net.conv3b.W.data, nobias=True),
 
-            l4 = L.Linear(None, 128, nobias=True),
-            l5 = L.Linear(128, 128, nobias=True),
-            l6 = L.Linear(128, n_out, nobias=True),
+            #  l4 = L.Linear(None, 128, nobias=True),
+            #  l5 = L.Linear(128, 128, nobias=True),
+            #  l6 = L.Linear(128, n_out, nobias=True),
+
+            l4 = L.Linear(None, 128, initialW=net.l4.W.data, nobias=True),
+            l5 = L.Linear(128, 128, initialW=net.l5.W.data, nobias=True),
+            l6 = L.Linear(128, n_out, initialW=net.l6.W.data, nobias=True),
         )
 
         self.k = k
